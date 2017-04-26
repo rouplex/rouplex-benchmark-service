9508626eac92f09237824d71dfcaaa9a0675e1b3
Andi Mullaraj <andimullaraj@gmail.com>
2017-04-26 02:28:36 -0700

    More detail on internal errors

--- benchmark-service-provider-jersey/config/ec2-benchmark-scripts/deploy.sh
+++ benchmark-service-provider-jersey/config/ec2-benchmark-scripts/deploy.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 download_setup() {
-    sudo yum install -y git
+    cd /home/ec2-user
     mkdir .ssh
+
     aws s3 cp s3://rouplex/deploys/access-keys/bb-rouplex-deploy.pem .ssh > /dev/null 2>&1
-    chmod 400 .ssh/bb-rouplex-deploy.pem
     aws s3 cp s3://rouplex/deploys/access-keys/known_hosts .ssh > /dev/null 2>&1
+
+    chmod 400 .ssh/bb-rouplex-deploy.pem
+    chown -R ec2-user:ec2-user .ssh
+
+    yum install -y git
     rm -rf rouplex-deploy-benchmark
-    ssh-agent bash -c "ssh-add .ssh/bb-rouplex-deploy.pem; git clone ssh://bitbucket.org/rouplex/rouplex-deploy-benchmark.git --branch $1 --single-branch" > /dev/null 2>&1
+    sudo -H -u ec2-user bash -c "ssh-agent bash -c 'ssh-add .ssh/bb-rouplex-deploy.pem; git clone ssh://bitbucket.org/rouplex/rouplex-deploy-benchmark.git --branch $1 --single-branch'"
 }
 
 download_setup master
-rouplex-deploy-benchmark/src/main/shell/common/ec2-setup.sh default
+sudo -H -u ec2-user bash -c rouplex-deploy-benchmark/src/main/shell/common/ec2-setup.sh default
